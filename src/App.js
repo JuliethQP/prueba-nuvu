6af1f27790498df4b7515725b8cc4d18a8c6db97
Julieth <juliethquinchia@gmail.com>
2020-11-16 18:43:29 -0500

    Update media query

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 
-import Header from './components/Header'
+import Body from './components/Body'
 import './Apps.scss'
 
 function App() {
   return (
     <div className="App">
-      <Header/>
+      <Body/>
     </div>
   );
 }
